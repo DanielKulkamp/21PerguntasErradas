0e2f13b14e0cbe10d79137170f9ebd3c8ffd0d31
Daniel Clemes Kulkamp <44503536+DanielKulkamp@users.noreply.github.com>
2018-12-06 21:41:12 -0200

    Add files via upload

--- 21Perguntas.js
+++ 21Perguntas.js
@@ -1,29 +1,62 @@
+var beee = new Audio('./beee.mp3');
+var errou = new Audio('./errou.mp3');
+var fuefuefue = new Audio('./fuefuefue.mp3');
+
 document.addEventListener('keydown', function(event){
     switch(event.keyCode){
     	case 38:
-    		alert("Up")
+    		if(listaDePerguntas[perguntaAtual].errado == $("#opcao1").text()){
+    			perguntaAtual++;
+    			mostraPergunta();
+    		} else {
+    			errou.play();
+    			voltaPergunta = perguntaAtual;
+    			perguntaAtual = 0;
+    			mostraPergunta();
+    		}
     		break;
     	case 40:
-    		alert("Down!")
+    		if(listaDePerguntas[perguntaAtual].errado == $("#opcao2").text()){
+    			perguntaAtual++;
+    			mostraPergunta();
+    		} else {
+    			errou.play();
+    			voltaPergunta = perguntaAtual;
+    			perguntaAtual = 0;
+    			mostraPergunta();
+    		}
     		break;
     	case 8:
-    		alert("Backspace!")
+    		voltaPergunta = perguntaAtual;
+    		perguntaAtual = 0;
+    		mostraPergunta();
     		break;
     }
 } );
+
+document.addEventListener('mousedown', function(event){
+    if (event.button == 2 && fase == "normal"){
+		beee.play();
+		voltaPergunta = perguntaAtual;
+    	perguntaAtual = 0;
+    	mostraPergunta();
+    }
+} );
+
+
 
 function Pergunta(certo, errado, pergunta) {
   this.certo = certo;
   this.errado = errado;
   this.pergunta = pergunta;
-  this.orde = ordem = Math.floor(Math.random() * 2) +1;
+  this.ordem = ordem = Math.floor(Math.random() * 2) +1;
 }
 
 var perguntasDemo = [
 	new Pergunta("4", "2", "Quanto é 2 + 2?"),
 	new Pergunta("Cabral", "Vespúcio", "A quem é atribuído o descobrimento do brasil?"),
 	new Pergunta("1", "2", "1"),
-	new Pergunta("2", "2", "2"),
+	new Pergunta("2", "3", "2"),
 	new Pergunta("3", "2", "3"),
 	new Pergunta("4", "2", "4"),
 	new Pergunta("5", "2", "5"),
@@ -47,8 +80,9 @@ var perguntasDemo = [
 var tempo = 210;
 var perguntaAtual = 0;
 var listaDePerguntas = [];
-
-
+var timerID = 0;
+var voltaPergunta = 0;
+var fase = "parado"
 
 
 function mostraPergunta() {
@@ -64,25 +98,30 @@ function mostraPergunta() {
 
 function mostraTempo(){
 	$("#minutos").html(""+Math.floor(tempo/60));
-	$("#segundos").html(""+tempo%60);
+	seg = tempo%60;
+	$("#segundos").html(""+(seg<10?"0":"")+tempo%60);
 }
 
 function timer() {
+	//alert("tempo: "+tempo);
+	//alert("t: "+t);
 	tempo--;
 	mostraTempo();
 	if (tempo == 0){
-		alert("Fim do tempo");
-	} else {
-		setTimeout(timer(), 1000);
-	}
+		clearInterval(timerID);
+		fuefuefue.play();
+	} 
 }
 
 var jogo = null;
 function iniciar(){
+	clearInterval(timerID);
+	fase = "normal";
 	tempo = 210
 	listaDePerguntas = perguntasDemo
 	mostraPergunta();
 	mostraTempo();
-	setTimeout(timer(),1000);
+	timerID = setInterval(timer,1000);
+	$("#message").html("timer ID:" + timerID);
 }
 
